981277f169efeddfad1280b63539ff8747d2d373
Hamdan Trisnawan <hamdantrisnawan697@gmail.com>
2026-02-16 23:25:08 +0700

    fix source

--- src/app/og/docs/[...slug]/route.tsx
+++ src/app/og/docs/[...slug]/route.tsx
@@ -1,4 +1,4 @@
-import { getPageImage, source } from '@/lib/source';
+import { getPageImage, docsSource } from '@/lib/source';
 import { notFound } from 'next/navigation';
 import { ImageResponse } from 'next/og';
 import { generate as DefaultImage } from 'fumadocs-ui/og';
@@ -7,7 +7,7 @@ export const revalidate = false;
 
 export async function GET(_req: Request, { params }: RouteContext<'/og/docs/[...slug]'>) {
   const { slug } = await params;
-  const page = source.getPage(slug.slice(0, -1));
+  const page = docsSource.getPage(slug.slice(0, -1));
   if (!page) notFound();
 
   return new ImageResponse(
@@ -20,7 +20,7 @@ export async function GET(_req: Request, { params }: RouteContext<'/og/docs/[...
 }
 
 export function generateStaticParams() {
-  return source.getPages().map((page) => ({
+  return docsSource.getPages().map((page) => ({
     lang: page.locale,
     slug: getPageImage(page).segments,
   }));
